2b5c38ffa1ba4be5845add2225e7f83412354f56
swetha15 <33092167+swetha15@users.noreply.github.com>
2017-11-21 15:08:26 +0530

    Add files via upload

---
+++ todaytask.py
@@ -0,0 +1,84 @@
+#Hello world program
+"""print("Hello world")"""
+
+#Add two numbers
+
+"""def add(a,b):
+    print(a+b)"""
+
+#fine square root
+
+"""def sqr(x):
+    print(x**2)"""
+
+#Area of triangle
+
+"""def trian(height,base):
+    area=height*base/2
+    return area"""
+#swapping two numbers
+
+"""def swap(a,b):
+    c=a
+    a=b
+    b=c
+    print(a,b)"""
+#random number
+
+"""import random
+def rand(n):
+    for i in range (n):
+        a=random.randint(1,10)
+        print(a)"""
+#convert km to mile
+"""kil=float(input ("enter km value"))
+convert=0.621371
+miles=kil*convert
+print(kil,miles)"""
+
+#fahrenhit to celsius
+
+"""cel=float (input("enter cel value"))
+far=(cel*1.8)+32
+print(cel,far)"""
+
+#positive,negative,zero
+
+"""def fun(x):
+    if x>0:
+        print("Positive num")
+    elif x==0:
+        print("entered num is zero")
+    else:
+        print("negative num")"""
+
+    
+#even and odd number
+"""def number(a):
+    if a%2==0:
+        print ("even")
+    else:
+        print ("odd")"""
+# leap year
+"""def year(x):
+    if x%4==0:
+        if x%100==0:
+             if x%400==0:
+                 print ("leap year")
+             else:
+                print ("not a leap year")
+        else:
+            print("leap year")
+    else:
+        print ("not a leap year")"""
+#find biggest number
+"""def biggest(a,b,c):
+    if(a>b) and (a>c):
+        print ("a biggest number")
+    elif (b>c) and (b>a):
+        print ("b biggest number")
+    else:
+        print("c biggest value")"""
+
+            
+      
